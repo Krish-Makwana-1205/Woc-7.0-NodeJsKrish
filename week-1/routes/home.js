a700920066372f0778a1eac574b45a2a098bb71a
Krish-Makwana-1205 <202301103@daiict.ac.in>
2025-01-10 19:50:26 +0530

    Email add delete and show integrated into a single page

--- week-1/routes/home.js
+++ week-1/routes/home.js
@@ -2,9 +2,11 @@ const express = require('express');
 const {putEmail, allEmail, deleteEmail} = require('../controller/CRUD');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.render('post');
-})
+// router.get('/', (req, res)=>{
+//     res.render('showall'.{
+//         items
+//     });
+// })
 router.get('/email',allEmail);
 router.post('/',putEmail);
 router.post('/delete', deleteEmail)
