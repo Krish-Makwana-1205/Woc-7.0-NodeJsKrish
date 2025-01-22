7e9f24380e252146a6b112a4ead8e6a57bd06ac8
Krish-Makwana-1205 <202301103@daiict.ac.in>
2025-01-22 15:54:49 +0530

    Week-2 tasks complete

--- project/view/ask-question.js
+++ project/view/ask-question.js
@@ -4,3 +4,10 @@ emailnav.addEventListener('click', emailrelocate);
 function emailrelocate(){
     window.location.href = "http://localhost:8002/email";
 }
+
+const logoutbtn = document.getElementById('logout');
+logoutbtn.addEventListener('click', Logout);
+function Logout(){
+    document.cookie = "uid=;";
+    location.reload();
+}
