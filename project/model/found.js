aaa1fa209b6d9a697063b4a4557701bfb9627c33
Krish-Makwana-1205 <202301103@daiict.ac.in>
2025-02-17 12:51:55 +0530

    Cron job added

--- project/model/found.js
+++ project/model/found.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const cron = require('node-cron');
+
 
 const lostschema = new mongoose.Schema({
     lostuser : {
@@ -22,6 +24,8 @@ const lostschema = new mongoose.Schema({
      }
 });
 
+
+
 const category = new mongoose.Schema({
     category:{
         type:String,
@@ -32,4 +36,18 @@ const category = new mongoose.Schema({
 
 const found = mongoose.model('found', category);
 
+cron.schedule('* * * * *', async() => {
+   const uno = new Date();
+   uno.setDate(uno.getDate() - 1);
+
+   try{
+      await found.updateMany(
+         { 'lost.createdAt': { $lt: uno } },
+         { $pull: { lost: { createdAt: { $lt: uno } } } }
+     );
+     console.log("deleted");
+   }catch(error){
+      console.log("error in cron job");
+   }
+});
 module.exports = found;
